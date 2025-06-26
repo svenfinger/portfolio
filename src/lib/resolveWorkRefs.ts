89ba6c82168e74a117b738fe3a17c68e3d451c43
svenfinger <hi@svenfinger.digital>
2025-06-26 22:15:25 +0200

    Add dynamic meta description support for work pages and improve meta data

--- src/lib/resolveWorkRefs.ts
+++ src/lib/resolveWorkRefs.ts
@@ -2,7 +2,9 @@ import { sanity } from './sanityClient';
 
 export async function resolveWorkRefs(refs: { _ref: string }[]) {
   if (!refs?.length) return [];
+
   const ids = refs.map((ref) => ref._ref);
   const query = `*[_type == "work" && _id in $ids]`;
+
   return await sanity.fetch(query, { ids });
 }
